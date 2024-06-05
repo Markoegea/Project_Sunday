0b5534660f9fc7764be7fb4b83fed085771b12a6
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2024-06-05 00:47:32 -0400

    Other_libraries_Import

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+import setuptools
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
-setup(
+setuptools.setup(
     name='sunday',
     version='0.9',
     description='System Using Neural Development Apis Yield',
@@ -12,10 +12,5 @@ setup(
     author='Marco Egea',
     author_email='markoegea@gmail.com',
     url="https://markoegea.onrender.com/",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        'License :: OSI Approved :: Apache 2.0 Software License',
-        "Operating System :: OS Independent",
-    ]
+    packages=setuptools.find_packages()
 )
