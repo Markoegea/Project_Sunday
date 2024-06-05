fac19003b5bbb93b3b22de542387f9a26e3c4046
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2024-06-05 00:34:43 -0400

    fixing_library_

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='sunday',
-   version='0.9',
-   description='System Using Neural Development Apis Yield',
-   license="Apache 2.0",
-   long_description=long_description,
-   author='Marco Egea',
-   author_email='markoegea@gmail.com',
-   url="https://markoegea.onrender.com/",
-   packages=['sunday'],  #same as name
-   install_requires=['joblib', 'numpy', 'scikit-learn', 'scipy', 'threadpoolctl'], #external packages as dependencies
+    name='sunday',
+    version='0.9',
+    description='System Using Neural Development Apis Yield',
+    license="Apache 2.0",
+    long_description=long_description,
+    author='Marco Egea',
+    author_email='markoegea@gmail.com',
+    url="https://markoegea.onrender.com/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache 2.0 Software License',
+        "Operating System :: OS Independent",
+    ]
 )
